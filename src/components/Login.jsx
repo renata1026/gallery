c80341b11ee68a367b4baa9a91ae49890b4c9a6f
Renata Reinartz <rcreinartz@gmail.com>
2023-09-13 10:09:18 -0600

    added-form-login-functionality

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,16 +1,49 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import './Paintings';
+import Paintings from './Paintings';
 
 const Login = () => {
+  const [userName, setUserName] = useState('');
+  const [password, setPassword] = useState('');
+  const [user, setUser] = useState({ id: 1, username: 'Daniel Smith' }); // Initialize with a dummy user
+
+  async function handleRegister(e) {
+    e.preventDefault();
+  }
+
+  function handleLogout() {
+    // Clear the user information when clicking "Logout"
+    setUser({});
+  }
+
+  console.log(userName, password);
+
   return (
     <>
       <div className="form-container">
-        <form>
-          <h1>Login</h1>
-          <input placeholder="Username"></input>
-          <input placeholder="Password"></input>
-          <button>Login</button>
-        </form>
+        {user.id ? (
+          <>
+            <p>Welcome, {user.username}!</p>
+            {/* <button onClick={handleLogout}>Logout</button> */}
+          </>
+        ) : (
+          <form onSubmit={handleRegister}>
+            <h1>Register</h1>
+            <input
+              onChange={(e) => setUserName(e.target.value)}
+              value={userName}
+              placeholder="Username"
+            ></input>
+            <input
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
+              placeholder="Password"
+            ></input>
+            <button>Register</button>
+          </form>
+        )}
       </div>
+      <Paintings />
     </>
   );
 };
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,15 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Register = () => {
+  const [userName, setUserName] = useState('');
+  const [password, setPassword] = useState('');
+  const [user, setUser] = useState({ id: 1, username: 'Daniel Smith' }); // Initialize with a dummy user
+
+  async function handleRegister(e) {
+    e.preventDefault();
+  }
+
+  function handleLogout() {
+    // Clear the user information when clicking "Logout"
+    setUser({});
+  }
+
+  console.log(userName, password);
+
   return (
     <>
       <div className="form-container">
-        <form>
-          <h1>Register</h1>
-          <input placeholder="Username"></input>
-          <input placeholder="Password"></input>
-          <button>Register</button>
-        </form>
+        {user.id ? (
+          <>{/* <button onClick={handleLogout}>Logout</button> */}</>
+        ) : (
+          <form onSubmit={handleRegister}>
+            <h1>Register</h1>
+            <input
+              onChange={(e) => setUserName(e.target.value)}
+              value={userName}
+              placeholder="Username"
+            ></input>
+            <input
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
+              placeholder="Password"
+            ></input>
+            <button>Register</button>
+          </form>
+        )}
       </div>
     </>
   );
